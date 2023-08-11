e0ab4db5e2e46e36f7fc9182c93a56e9d0e30536
MFDGaming <alexandros.argentakis@gmail.com>
2023-08-11 13:11:54 +0300

    One more multitouch fix

--- src/client/input/Multitouch.h
+++ src/client/input/Multitouch.h
@@ -18,35 +18,35 @@ public:
     static bool _wasReleased[12];
     static bool _wasReleasedThisUpdate[12];
 
-    int32_t _clampPointerId(int32_t pointerId);
+    static int32_t _clampPointerId(int32_t pointerId);
 
-    void commit();
+    static void commit();
 
-    void feed(uint8_t button, uint8_t buttonState, uint16_t x, uint16_t y, uint8_t pointerId);
+    static void feed(uint8_t button, uint8_t buttonState, uint16_t x, uint16_t y, uint8_t pointerId);
     
-    MouseDevice *g(int32_t pointerId);
+    static MouseDevice *g(int32_t pointerId);
 
-    int32_t getActivePointerIds(int32_t **pPointers);
+    static int32_t getActivePointerIds(int32_t **pPointers);
 
-    MouseAction *getEvent();
+    static MouseAction *getEvent();
 
-    int32_t getFirstActivePointerIdEx();
+    static int32_t getFirstActivePointerIdEx();
 
-    int32_t getFirstActivePointerIdExThisUpdate();
+    static int32_t getFirstActivePointerIdExThisUpdate();
 
-    int16_t getX(int32_t pointerId);
+    static int16_t getX(int32_t pointerId);
 
-    int16_t getY(int32_t pointerId);
+    static int16_t getY(int32_t pointerId);
 
-    bool isPressed(int32_t pointerId);
+    static bool isPressed(int32_t pointerId);
 
-    bool next();
+    static bool next();
 
-    void reset();
+    static void reset();
 
-    void resetThisUpdate();
+    static void resetThisUpdate();
 
-    void rewind();
+    static void rewind();
 };
 
 #endif
