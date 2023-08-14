79f9d5651410e29ea59b0828b7da1fb3d15cdd8f
MFDGaming <alexandros.argentakis@gmail.com>
2023-08-14 20:18:52 +0300

    Some UnifiedTurnBuild progress

--- src/client/input/UnifiedTurnBuild.cpp
+++ src/client/input/UnifiedTurnBuild.cpp
@@ -28,7 +28,13 @@ UnifiedTurnBuild::UnifiedTurnBuild(int32_t xx, int32_t width, int32_t height, fl
 }
 
 float UnifiedTurnBuild::getSpeedSquared(void *entity) {
-    return 0.0f; // TODO
+    /*
+        float speedX = entity->x - entity->motionX; // entity+1 - entity+15
+        float speedY = entity->y - entity->motionY; // entity+2 - entity+16
+        float speedZ = entity->z - entity->motionZ; // entity+3 - entity+17
+        return (speedX * speedX) + (speedY * speedY) + (speedZ * speedZ);
+    */
+    return 0.0f;
 }
 
 TurnDelta UnifiedTurnBuild::getTurnDelta() {
