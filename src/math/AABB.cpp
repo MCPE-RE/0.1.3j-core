67cd2736df34a14e20656764e7fb16efb64bd69e
MFDGaming <alexandros.argentakis@gmail.com>
2023-08-16 13:30:09 +0300

    Some AABB progress

--- src/math/AABB.cpp
+++ src/math/AABB.cpp
@@ -14,32 +14,140 @@ HitResult AABB::clip(const Vec3& vector1, const Vec3& vector2) {
     return result;
 }
 
-void AABB::clipXCollide(const AABB& aabb, float x) {}
+float AABB::clipXCollide(const AABB& aabb, float x) {}
 
-void AABB::clipYCollide(const AABB& aabb, float y) {}
+float AABB::clipYCollide(const AABB& aabb, float y) {}
 
-void AABB::clipZCollide(const AABB& aabb, float z) {}
+float AABB::clipZCollide(const AABB& aabb, float z) {}
 
-void AABB::cloneMove(float x, float y, float z) {}
+AABB AABB::cloneMove(float x, float y, float z) {
+    return AABB::newTemp(
+        this->minX + x,
+        this->minY + y,
+        this->minZ + z,
+        this->maxX + x,
+        this->maxY + y,
+        this->maxZ + z
+    );
+}
 
-void AABB::contains(const Vec3& vector) {}
+bool AABB::contains(const Vec3& vector) {
+    if (vector.x <= this->minX || vector.x >= this->maxX) {
+        return false;
+    }
+    if (vector.y > this->minY && vector.y < this->maxY) {
+        if (vector.z > this->minZ && vector.z < this->maxZ) {
+            return true;
+        }
+    }
+    return false;
+}
     
-void AABB::containsX(Vec3 *vector) {}
+bool AABB::containsX(Vec3 *vector) {
+    return (
+        vector->y >= this->minY &&
+        vector->y <= this->maxY &&
+        vector->z >= this->minZ &&
+        vector->z <= this->maxZ
+    );
+}
 
-void AABB::containsY(Vec3 *vector) {}
+bool AABB::containsY(Vec3 *vector) {
+    return (
+        vector->x >= this->minX &&
+        vector->x <= this->maxX &&
+        vector->z >= this->minZ &&
+        vector->z <= this->maxZ
+    );
+}
 
-void AABB::containsZ(Vec3 *vector) {}
+bool AABB::containsZ(Vec3 *vector) {
+    return (
+        vector->x >= this->minX &&
+        vector->x <= this->maxX &&
+        vector->y >= this->minY &&
+        vector->y <= this->maxY
+    );
+}
 
-void AABB::expand(float x, float y, float z) {}
+AABB AABB::expand(float x, float y, float z) {
+    return AABB::newTemp(
+        x < 0.0f ? (this->minX + x) : this->minX,
+        x > 0.0f ? (this->maxX + x) : this->maxX,
+        x < 0.0f ? (this->minY + y) : this->minY,
+        x > 0.0f ? (this->maxY + y) : this->maxY,
+        x < 0.0f ? (this->minZ + z) : this->minZ,
+        x > 0.0f ? (this->maxZ + z) : this->maxZ
+    );
+}
 
-float AABB::getSize() {}
+float AABB::getSize() {
+    float dx = this->maxX - this->minX;
+    float dy = this->maxY - this->minY;
+    float dz = this->maxZ - this->minZ;
+    return (dx + dy + dz) / 3.0f;
+}
 
-void AABB::grow(float x, float y, float z) {}
+AABB AABB::grow(float x, float y, float z) {
+    return AABB(
+        this->minX - x,
+        this->minY - y,
+        this->minZ - z,
+        this->maxX + x,
+        this->maxY + y,
+        this->maxZ + z
+    );
+}
 
-void AABB::intersects(const AABB& aabb) {}
+bool AABB::intersects(const AABB& aabb) {
+    if (this->minX >= aabb.maxX || this->maxX <= aabb.minX) {
+        return false;
+    }
+    if (this->minY < aabb.maxY && this->maxY > aabb.minY) {
+        if (this->minZ < aabb.maxZ && this->maxZ > aabb.minZ) {
+            return true;
+        }
+    }
+}
 
-void AABB::move(float x, float y, float z) {}
+bool AABB::intersects(float minX, float minY, float minZ, float maxX, float maxY, float maxZ) {
+    if (this->minX >= maxX || this->maxX <= minX) {
+        return false;
+    }
+    if (this->minY < maxY && this->maxY > minY) {
+        if (this->minZ < maxZ && this->maxZ > minZ) {
+            return true;
+        }
+    }
+}
 
-void AABB::set(const AABB& aabb) {}
+void AABB::move(float x, float y, float z) {
+    this->minX += x;
+    this->minY += y;
+    this->minZ += z;
+    this->maxX += x;
+    this->maxY += y;
+    this->maxZ += z;
+}
 
-void AABB::set(float x1, float y1, float z1, float x2, float y2, float z2) {}
+AABB AABB::newTemp(float minX, float minY, float minZ, float maxX, float maxY, float maxZ) {
+    return AABB(minX, minY, minZ, maxX, maxY, maxZ);
+}
+
+void AABB::set(const AABB& aabb) {
+    this->minX = aabb.minX;
+    this->minY = aabb.minY;
+    this->minZ = aabb.minZ;
+    this->maxX = aabb.maxX;
+    this->maxY = aabb.maxY;
+    this->maxZ = aabb.maxZ;
+}
+
+void AABB::set(float minX, float minY, float minZ, float maxX, float maxY, float maxZ) {
+    this->minX = minX;
+    this->minY = minY;
+    this->minZ = minZ;
+    this->maxX = maxX;
+    this->maxY = maxY;
+    this->maxZ = maxZ;
+}
--- src/math/AABB.h
+++ src/math/AABB.h
@@ -15,33 +15,37 @@ public:
 
     HitResult clip(const Vec3& vector1, const Vec3& vector2);
 
-    void clipXCollide(const AABB& aabb, float x);
+    float clipXCollide(const AABB& aabb, float x);
 
-    void clipYCollide(const AABB& aabb, float y);
+    float clipYCollide(const AABB& aabb, float y);
 
-    void clipZCollide(const AABB& aabb, float z);
+    float clipZCollide(const AABB& aabb, float z);
 
-    void cloneMove(float x, float y, float z);
+    AABB cloneMove(float x, float y, float z);
 
-    void contains(const Vec3& vector);
+    bool contains(const Vec3& vector);
     
-    void containsX(Vec3 *vector);
+    bool containsX(Vec3 *vector);
 
-    void containsY(Vec3 *vector);
+    bool containsY(Vec3 *vector);
 
-    void containsZ(Vec3 *vector);
+    bool containsZ(Vec3 *vector);
 
-    void expand(float x, float y, float z);
+    AABB expand(float x, float y, float z);
 
     float getSize();
 
-    void grow(float x, float y, float z);
+    AABB grow(float x, float y, float z);
 
-    void intersects(const AABB& aabb);
+    bool intersects(const AABB& aabb);
+
+    bool intersects(float minX, float minY, float minZ, float maxX, float maxY, float maxZ);
 
     void move(float x, float y, float z);
 
+    static AABB newTemp(float minX, float minY, float minZ, float maxX, float maxY, float maxZ);
+
     void set(const AABB& aabb);
 
-    void set(float x1, float y1, float z1, float x2, float y2, float z2);
+    void set(float minX, float minY, float minZ, float maxX, float maxY, float maxZ);
 };
