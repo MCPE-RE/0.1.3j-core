248ea866f216a38894cdac01b64194730dbe21be
MFDGaming <alexandros.argentakis@gmail.com>
2023-09-16 07:40:11 +0300

    Added the DirtTile

---
+++ src/tile/DirtTile.cpp
@@ -0,0 +1,3 @@
+#include "DirtTile.h"
+
+DirtTile::DirtTile(int32_t resource, int32_t texture) : Tile(resource, texture, Material::dirt) {}
---
+++ src/tile/DirtTile.h
@@ -0,0 +1,6 @@
+#include "Tile.h"
+
+class DirtTile : public Tile {
+public:
+    DirtTile(int32_t resource, int32_t texture);
+};
